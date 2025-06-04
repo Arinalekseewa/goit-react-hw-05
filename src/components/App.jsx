c1a61aa366c2321e53cefe61d8b0c884c463312d
Ariel <arina.kudar@gmail.com>
2025-06-04 12:46:41 +0300

    update

---
+++ src/components/App.jsx
@@ -0,0 +1,15 @@
+
+
+const ACCESS_KEY = 'zT7iurgOs4_wWfKlD0Z4NaERRPNbOdVeoV2UmNzaHyA';
+
+export default function App() {
+
+
+  return (
+    <>
+      <div>
+      </div>
+      
+    </>
+  )
+};
